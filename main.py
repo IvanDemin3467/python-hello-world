db5eb8552f0b7fe1bfa214ace347fe42e00a77a7
IvanDemin3467 <IvanDemin3467@gmail.com>
2026-08-14 16:51:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+﻿from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+import os
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        body = b"hello world\n"
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        self.send_header("Content-Length", str(len(body)))
+        self.end_headers()
+        self.wfile.write(body)
+
+    def log_message(self, format, *args):
+        return
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", "8000"))
+    server = ThreadingHTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
